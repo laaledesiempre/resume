cdd83faa7df9aef30a71bc06d6f7586f4869c018
laaledesiempre <128645176+laaledesiempre@users.noreply.github.com>
2023-04-14 17:31:39 -0300

    icons added

--- data.js
+++ data.js
@@ -3,32 +3,32 @@ const generalDescription="I am a sensitive and thoughtful person who always look
 const aboutMe="I want to make a career in a company where my work can help costumers, and lead my co-workers to be the best they can ever be"
 const contacts=[
     {
-        icon: "",
+        icon: "fa-brands fa-linkedin",
         contact:"https://www.linkedin.com/in/fernando-paj%C3%B3n-43214626b",
     },
     {
-        icon: "",
+        icon: "fa-brands fa-telegram",
         contact:"1176177910",
     },
     {
-        icon: "",
+        icon: "fa-solid fa-square-envelope",
         contact:"alejandrabotacura@outlook.com.ar",
     },
 ]
 const abouts=[
     {
         name: "Profile",
-        icon: "",
+        icon: "fa-sharp fa-solid fa-address-book",
         content:"I am interested about progress every day, Developing myself in my workspace based on good performance, attitude, and integration with colleagues."
     },
     {
         name: "Performance",
-        icon: "",
+        icon: "fa-solid fa-briefcase",
         content:"I am active and organized, I used to look for solutions and always prioritize objetives above anything else. What I enjoy the most is coworking, and sharing knowledge acquired through experience"
     },
     {
         name: "Looking for",
-        icon: "",
+        icon: "fa-solid fa-magnifying-glass",
         content:"a work where i will be constantly learning, developing projects that require creativity in solving problems, improving my skills every day, somewhere i can be proud of working at"
     },
 
@@ -36,42 +36,42 @@ const abouts=[
 const technologies=[
     {
         name:"JavaScript",
-        icon:"",
+        icon:"fa-brands fa-js",
         meter: 6
     },
     {
         name:"HTML",
-        icon:"",
+        icon:"fa-brands fa-html5",
         meter: 6
     },
     {
         name:"CSS",
-        icon:"",
+        icon:"fa-brands fa-css3-alt",
         meter: 5
     },
     {
         name:"React.js",
-        icon:"",
+        icon:"fa-brands fa-react",
         meter: 6
     },
     {
         name:"React Native",
-        icon:"",
+        icon:"fa-solid fa-mobile-notch",
         meter: 2
     },
     {
         name:"Electron",
-        icon:"",
+        icon:"fa-sharp fa-regular fa-desktop",
         meter: 2
     },
     {
         name:"Adobe Illustrator",
-        icon:"",
+        icon:"fa-solid fa-vector-square",
         meter: 8
     },
     {
         name:"Adobe Photoshop",
-        icon:"",
+        icon:"fa-solid fa-image",
         meter: 7
     },
 ]
@@ -85,38 +85,38 @@ const certifications=[
     {
         name:"FreeCodeCamp certification in Javascript Data Structures and Algorithms",
         status:"Finished",
-        icon:"",
+        icon:"fa-brands fa-free-code-camp",
         content:"Javascript Data Structures, methods and algorithms, you can see the certification here: https://www.freecodecamp.org/fcc5ecf56b1-506d-4112-b573-2d5f5a975f31"
     },
     {
         name:"FreeCodeCamp certification in FrontEnd Development Libraries",
         status:"currently studying",
-        icon:"",
+        icon:"fa-brands fa-free-code-camp",
         content:"Practical course about Bootstrap, JQuery, React.js and Redux, Redux/Toolkit, you can see my progress here:  https://www.freecodecamp.org/fcc5ecf56b1-506d-4112-b573-2d5f5a975f31"
     },
     {
         name:"Practical Graphic Design certification in CorelDraw, CorelPhotoPaint, Adobe Illustrator and Photoshop ",
         status:"Finished",
-        icon:"",
+        icon:"fa-regular fa-book-open",
         content:"Practical course about design of vector images and bitmaps."
     },
 ]
 const experience=[
     {
         name:"Arco Maquinarias",
-        logo:"",
+        logo:"fa-regular fa-screwdriver-wrench",
         summary:"[2019-2020] Worked on dispatch and stock controll",
         tokens:["Responsability","Hard Work"],
     },
     {
         name:"Distribuidora Luciano",
-        logo:"",
+        logo:"fa-solid fa-shop",
         summary:"[2020-2021] Worked on costumer support, and shop management",
         tokens:["Responsability","Co-working","Leading"],
     },
     {
         name:"Arco Maquinarias",
-        logo:"",
+        logo:"fa-regular fa-screwdriver-wrench",
         summary:"[2021-currently] Working as web assistant, e-commerce customer service, payment management, replacement cashier, replacement Mercado Libre (e-commerce platform) management",
         tokens:["Responsability","Co-working","Multitasking"],
     },
